bc8c7af6ebb0b27e112b334d687d43c7722d3a6a
Aril-Lamay <72742540+Aril-Lamay@users.noreply.github.com>
2022-05-31 09:06:29 +0530

    Add files via upload

--- uploadFiles.py
+++ uploadFiles.py
@@ -1,34 +1,37 @@
 import os
-from posixpath import relpath
 import dropbox
 from dropbox.files import WriteMode
-
+#
 class TransferData:
     def __init__(self,access_token):
-        self.access_token = access_token
-
-    def upload_files(self,file_from,file_to):
+        self.access_token =  access_token
+#
+    def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
 
-        for root,dirs,files in os.walk(file_from):
+            # enumerate local files recursively
+        for root, dirs, files in os.walk(file_from):
 
             for filename in files:
-                local_path = os.path.join(root,filename)
-            
-            realtive_path = os.path.relpath(local_path,file_from)
-            dropbox_path = os.path.join(file_to,realtive_path)
+                    # construct the full local path
+                local_path = os.path.join(root, filename)
 
-            with open(local_path,'rb') as f:
-                dbx.files_upload(f.read(),dropbox_path,mode=WriteMode('overwrite'))
+                    # construct the full Dropbox path
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                    # upload the file
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
 
 def main():
-    access_token = 'sl.BIkEHYQtsN1OAH_u2cdxYpQwJ7pTjOvxWZ_qP_Fcluiho-aeLbrsdBizYWKDkZb9Nk0rnQYL8I1Cx8tqiFEOrzDEr9UWyDo_QlXYaCKmxPd0WuB2i5Su57I9fpcckIlBxQwlEK2LSAbK'
+    access_token = 'sl.BIlz8kQjLNzQ1jbOkWy9LCc5np6H6Eg0moRCQpzgbEI0JxcEBCaWNe9PDsftl-FszEPI1tRh2ogI-qZvcHSi_TD4lFgl2g3n0oIQTyup9uCkkBS1bWQWV_OaLCQrFK_HeLJeuKteowcC'
     transferData = TransferData(access_token)
 
-    file_from = '/Users/arill/Documents/NewFolder/'
-    file_to = '/Test/'
+    file_from = str('/Users/arill/Documents/NewFolder')
+    file_to = '/Home/Test'
 
-    transferData.upload_files(file_from, file_to)
-    print("file has been moved")
+    # API v2
+    transferData.upload_file(file_from,file_to)
+    print("file has been moved !!!")
 
 main()
